1b9559ba8a54e5cf401fd60b0e188f216bed0aae
Ryan Nazareth <ryankarlos@gmail.com>
2025-01-02 03:01:24 +0000

    add autoscaling

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -58,3 +58,33 @@ resource "aws_ecs_service" "ecs_service" {
   }
 }
 
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 0
+  resource_id        = "service/${aws_ecs_service.ecs_service.cluster}/${aws_ecs_service.ecs_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+
+
+
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "alb_request_target_tracking"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
